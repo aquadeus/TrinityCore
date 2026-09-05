86af40e1406a14e63c5d8316b65784e922da1fd9
Ovahlord <18347559+ovahlord@users.noreply.github.com>
2026-09-05 18:05:27 +0200

    Core/Units: buildfix

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -13454,7 +13454,7 @@ bool Unit::SetDisableGravity(bool disable, bool updateAnimTier /*= true*/, bool 
         SendMessageToSet(packet.Write(), true);
     }
 
-    if (playHoverAnim && !GetVehicle())
+    if (updatePlayHoverAnim && !GetVehicle())
     {
         if (IsAlive())
         {
